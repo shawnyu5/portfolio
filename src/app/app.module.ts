3a823156d8fc1659814b21e57346872fdf8ae47b
Shawn Yu <shawn.yu5@hotmail.com>
2022-03-04 15:18:55 -0500

    style: bootwatch installed
    Dark bootstrap theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import "bootswatch/dist/[theme]/bootstrap.min.css";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavBarComponent } from "./nav-bar/nav-bar.component";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavBarComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [AppComponent, NavBarComponent],
+   imports: [BrowserModule, AppRoutingModule, NgbModule],
+   exports: [NavBarComponent],
+   providers: [],
+   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,25 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { DebugElement } from "@angular/core";
+import { NavBarComponent } from "./nav-bar.component";
+import { By } from "@angular/platform-browser";
+import { elementAt } from "rxjs";
 
-import { NavBarComponent } from './nav-bar.component';
+describe("NavBarComponent", () => {
+   let component: NavBarComponent;
+   let fixture: ComponentFixture<NavBarComponent>;
+   let debugElement: DebugElement[];
+   let htmlELement: HTMLElement;
 
-describe('NavBarComponent', () => {
-  let component: NavBarComponent;
-  let fixture: ComponentFixture<NavBarComponent>;
+   beforeEach(async () => {
+      await TestBed.configureTestingModule({
+         declarations: [NavBarComponent],
+      }).compileComponents();
+   });
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ NavBarComponent ]
-    })
-    .compileComponents();
-  });
+   beforeEach(() => {
+      fixture = TestBed.createComponent(NavBarComponent);
+      component = fixture.componentInstance;
+      fixture.detectChanges();
+      component = fixture.componentInstance;
+      debugElement = fixture.debugElement.queryAll(By.css(".nav-item"));
+      // htmlELement = debugElement[0].nativeElement;
+   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NavBarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+   it("should create", () => {
+      expect(component).toBeTruthy();
+   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+   it("should contain correct element text", () => {
+      let barText = ["Home", "Projects", "Contact"];
+      // console.log(htmlELement.textContent);
+      let index = 0;
+      debugElement.forEach((element) => {
+         expect(element.nativeElement.textContent).toMatch(barText[index]);
+         index++;
+      });
+   });
 });
