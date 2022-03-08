12e41687dd3298bede1994c3c060e4355f6bbea9
Shawn Yu <shawn.yu5@hotmail.com>
2022-03-07 21:23:56 -0500

    update

--- src/app/home-page/home-page.component.spec.ts
+++ src/app/home-page/home-page.component.spec.ts
@@ -1,16 +1,19 @@
-import { DebugElement } from "@angular/core";
+import { DebugElement, inject } from "@angular/core";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HomePageService } from "./home-page.service";
 
 import { HomePageComponent } from "./home-page.component";
+import { By } from "@angular/platform-browser";
 
 describe("HomePageComponent", () => {
    let component: HomePageComponent;
    let fixture: ComponentFixture<HomePageComponent>;
-   let debugElement: DebugElement[];
+   let debugElement: DebugElement;
 
    beforeEach(async () => {
       await TestBed.configureTestingModule({
          declarations: [HomePageComponent],
+         providers: [HomePageService],
       }).compileComponents();
    });
 
@@ -18,6 +21,7 @@ describe("HomePageComponent", () => {
       fixture = TestBed.createComponent(HomePageComponent);
       component = fixture.componentInstance;
       fixture.detectChanges();
+      debugElement = fixture.debugElement.query(By.css("#intro"));
    });
 
    it("should create", () => {
