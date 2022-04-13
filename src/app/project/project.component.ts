f148355dc88d19d62cb3a354b0d48c36c81b7026
shawn yu <shawn.yu5@hotmail.com>
2022-04-13 17:10:24 -0400

    fix: made fix gaps on card

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -5,7 +5,7 @@ import { ProjectService } from "../project.service";
 @Component({
    selector: "app-project",
    templateUrl: "./project.component.html",
-   styleUrls: ["./project.component.scss"],
+   styleUrls: ["./project.component.css"],
 })
 export class ProjectComponent implements OnInit {
    projects: Array<IProject> = [{}] as Array<IProject>;
