8987641640c789295f2fa8796e09792a77e65a8e
shawn yu <shawn.yu5@hotmail.com>
2022-04-16 19:20:13 -0400

    fix: weird boot strap issue causing `ng build` to fail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,34 +3,26 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { ProjectComponent } from "./project/project.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { NavBarComponent } from "./nav-bar/nav-bar.component";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatChipsModule } from "@angular/material/chips";
 import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      NavBarComponent,
-      HomePageComponent,
-      ProjectComponent,
-   ],
+   declarations: [AppComponent, HomePageComponent, ProjectComponent],
    imports: [
       BrowserModule,
       AppRoutingModule,
-      NgbModule,
       BrowserAnimationsModule,
       MatCardModule,
       MatToolbarModule,
       MatChipsModule,
       FlexLayoutModule,
    ],
-   exports: [NavBarComponent],
+   exports: [],
    providers: [],
    bootstrap: [AppComponent],
 })
