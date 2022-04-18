851e2464d6c5152891d5f625c500834ae5c72f26
shawn yu <shawn.yu5@hotmail.com>
2022-04-18 11:55:15 -0400

    installed ng bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatChipsModule } from "@angular/material/chips";
 import { MatCardModule } from "@angular/material/card";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
    declarations: [AppComponent, HomePageComponent, ProjectComponent],
@@ -21,6 +22,7 @@ import { MatCardModule } from "@angular/material/card";
       MatToolbarModule,
       MatChipsModule,
       FlexLayoutModule,
+      NgbModule,
    ],
    exports: [],
    providers: [],
