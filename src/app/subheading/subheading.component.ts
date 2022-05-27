9a774d56b4e4f706bd6273abc7c45b9c23ccad72
Shawn Yu <55592708+shawnyu5@users.noreply.github.com>
2022-05-26 21:00:36 -0400

    feat: responsive design (#12)

---
+++ src/app/subheading/subheading.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+   selector: "app-subheading",
+   templateUrl: "./subheading.component.html",
+   styleUrls: ["./subheading.component.scss"],
+})
+export class SubheadingComponent implements OnInit {
+   constructor() {}
+
+   ngOnInit(): void {}
+}
