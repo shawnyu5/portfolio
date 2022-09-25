718d08dfdd5424fb8ce7377ceec91ab8a68c5463
shawn yu <shawn.yu5@hotmail.com>
2022-09-25 13:37:35 -0400

    feat: github logo component
    Create a component to render a github logo in unicode

---
+++ src/app/git-hub-logo/git-hub-logo.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GitHubLogoComponent } from './git-hub-logo.component';
+
+describe('GitHubLogoComponent', () => {
+  let component: GitHubLogoComponent;
+  let fixture: ComponentFixture<GitHubLogoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GitHubLogoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GitHubLogoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/git-hub-logo/git-hub-logo.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+   selector: "git-hub-logo",
+   templateUrl: "./git-hub-logo.component.html",
+   styleUrls: ["./git-hub-logo.component.scss"],
+})
+export class GitHubLogoComponent implements OnInit {
+   constructor() { }
+
+   ngOnInit(): void { }
+}
