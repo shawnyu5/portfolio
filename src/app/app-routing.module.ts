5f07aa849c5a1976ee5f4123dcc5d415a2ba51f8
shawn yu <shawn.yu5@hotmail.com>
2022-10-16 15:21:43 -0400

    feat: init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { StupidHacksPresentationComponent } from "./stupid-hacks-presentation/stupid-hacks-presentation.component";
 
-const routes: Routes = [{ path: "", component: HomePageComponent }];
+const routes: Routes = [
+   { path: "", component: HomePageComponent },
+   { path: "hacks", component: StupidHacksPresentationComponent },
+];
 
 @NgModule({
    imports: [RouterModule.forRoot(routes)],
---
+++ src/app/stupid-hacks-presentation/stupid-hacks-presentation.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+   selector: "app-stupid-hacks-presentation",
+   templateUrl: "./stupid-hacks-presentation.component.html",
+   styleUrls: ["./stupid-hacks-presentation.component.scss"],
+})
+export class StupidHacksPresentationComponent implements OnInit {
+   html: string = "";
+   constructor(private http: HttpClient) { }
+
+   ngOnInit(): void {
+      this.http
+         .get("src/static-pages/stupid_hackathon_2022/presentation.html")
+         .subscribe((data) => {
+            console.log(data);
+         });
+   }
+}
